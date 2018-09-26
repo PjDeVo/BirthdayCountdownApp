37c7705445f5f73c4062527af82254009e4efd8f
patdevincentis <devincentis529@gmail.com>
2018-09-26 15:13:13 -0400

    added styles to clock with css grid and other styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,21 +4,23 @@ import Button from './button';
 import Clock from './clock';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className="grid">
-        <hi className="grid__title"> Birthday Countdown </hi>
-        <div className="grid__skew-dark-one-box"> </div>
-        <div className="grid__skew-dark-two"> </div>
-        <div className="grid__skew-dark-three"> </div>
-        <div className="grid__skew-light-one"> </div>
-        <div className="grid__skew-light-two"> </div>
-        <div className="grid__skew-light-three-box"> </div>
+    render() {
 
-        <Picker />
-        {Button('Generate Countdown')}
-        <Clock />
-      </div>
-    );
-  }
+        //return <div classname=""><Clock /> </div>
+        return (
+            <div className="grid">
+                <hi className="grid__title"> Birthday Countdown </hi>
+                <div className="grid__skew-dark-one-box"> </div>
+                <div className="grid__skew-dark-two"> </div>
+                <div className="grid__skew-dark-three"> </div>
+                <div className="grid__skew-light-one"> </div>
+                <div className="grid__skew-light-two"> </div>
+                <div className="grid__skew-light-three-box"> </div>
+
+                <Picker />
+                {Button('Generate Countdown')}
+                <Clock />
+            </div>
+        );
+    }
 }
